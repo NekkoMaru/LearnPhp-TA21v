64f207f3c59cf5de0e3399e159fc9647fd36a7f6
Nikita Izovit <Nikita.izovit@tptlive.ee>
2024-01-26 08:55:49 +0200

    Rework

---
+++ views/about.php
@@ -0,0 +1,6 @@
+<?php include __DIR__ . '/partials/header.php' ?> 
+<center>
+    <h1>In progress ...</h1>
+    <h2>Please, come later!</h2>
+</center>
+<?php include __DIR__ . '/partials/footer.php' ?> 
